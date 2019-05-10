927eb59c83d4b83888bdf5affcac6d87a207fd07
Emil-Ebrahimi <emil.ebrahimi@hotmail.com>
2019-05-10 15:14:51 +0200

    3 klassen
    3 klassen toegevoegd

---
+++ inschrijvingen.sql
@@ -0,0 +1,6 @@
+USE `lo8e_sql2`;
+INSERT INTO klassen 
+(klas_code, slb_code, cohort, opleiding_code)
+VALUES ('l09e-amo1', 'rs001', 2019, 'amo'),
+('l09e-amo2', 'brc01', 2019, 'amo'),
+('l09e-amo3', 'vlt03', 2019, 'amo');
