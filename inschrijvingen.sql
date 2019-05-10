97bcca0a255aa45a0a40c5a784eb3b38c7b6af91
Emil-Ebrahimi <emil.ebrahimi@hotmail.com>
2019-05-10 15:25:25 +0200

    Studenten
    4 nieuwe studenten

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -4,3 +4,10 @@ INSERT INTO klassen
 VALUES ('l09e-amo1', 'rs001', 2019, 'amo'),
 ('l09e-amo2', 'brc01', 2019, 'amo'),
 ('l09e-amo3', 'vlt03', 2019, 'amo');
+
+INSERT INTO studenten 
+(ov_nummer, achternaam, tussenvoegsel, voornaam)
+VALUES (90111, 'Stinson','','Zacky'),
+(90222,'Appels','','Koop'),
+(90333,'Azulu','','Abu'),
+(90444,'Svert','','Schwuen');
